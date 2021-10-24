712f5b6e29213540d12ecc913e05172c68e3b052
Lakshya Dhariwal <lakshyadhariwal9@gmail.com>
2021-10-24 23:50:27 +0530

    first commit

---
+++ switchExample.js
@@ -0,0 +1,12 @@
+const gender = "male";
+
+switch (gender) {
+  case "male":
+    console.log("Gender is Male");
+    break;
+  case "female":
+    console.log("Gender is Female");
+    break;
+  default:
+    console.log("Enter gender in male/female format");
+}
